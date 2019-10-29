2d18b384d07bf1c3ca115f52d062fbf8f28884cc
belbahrim <belbahrim@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-10-29 18:08:20 +0000

    Modified BestFeatures and LassoPath
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tools4uplift@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/BestFeatures.R
+++ pkg/R/BestFeatures.R
@@ -1,83 +1,93 @@
-BestFeatures <- function(data, treat, outcome, predictors, nb.lambda = 100, 
-                         rank.precision = 2, equal.intervals = FALSE, nb.group = 10, 
-                         validation = FALSE, p = 0.3, value = FALSE){
-
-  # Feature selection for the interaction estimator.
-  #
-  # Args:
-  #   data: a data frame containing the treatment, the outcome and the predictors.
-  #   treat: name of a binary (numeric) vector representing the treatment 
-  #          assignment (coded as 0/1).
-  #   outcome: name of a binary response (numeric) vector (coded as 0/1).
-  #   predictors: a vector of names representing the predictors to consider in the model.
-  #   ... and default parameters.
-  #
-  # Returns:
-  #   The best features for the interaction estimator.
+BestFeatures <- function (data, treat, outcome, predictors, rank.precision = 2, 
+                            equal.intervals = FALSE, nb.group = 10, 
+                            validation = TRUE, p = 0.3, value = FALSE) {
+    # Feature selection for the interaction estimator.
+    #
+    # Args:
+    #   data: a data frame containing the treatment, the outcome and the predictors.
+    #   treat: name of a binary (numeric) vector representing the treatment 
+    #          assignment (coded as 0/1).
+    #   outcome: name of a binary response (numeric) vector (coded as 0/1).
+    #   predictors: a vector of names representing the predictors to consider in the model.
+    #   ... and default parameters.
+    #
+    # Returns:
+    #   The best features for the interaction estimator.
   
   
-  # All variables must be continuous. Before using this function, change 
-  # categorical variables to dummies.
-  inter.formula <- c()
-  for (k in seq(1:length(predictors))) {
-    inter.formula <- paste(inter.formula, paste(predictors[k], treat, sep = ":"), sep="+")
-  }
-  formula <- as.formula(paste(paste(outcome, "~", treat, "+"), paste(predictors, collapse="+"), inter.formula))
-  
-  
-  # Cross-validation
-  # Split data between train (data) and valid using SplitUplift() function
-  if (validation == TRUE) {
-    split <- SplitUplift(data, 1-p, c(treat, outcome))
-    data <- split[[1]]
-    valid <- split[[2]]
-  }
-  
-  path <- LassoPath(data, formula, nb.lambda)
-  # Keep paths of dimension > 0
-  path <- path[path[, "dimension"] > 0, ]
-  
-  lambda.qini <- c()
-  
-  for (k in 1:nrow(path)) {
-    features <- path[k, -c(1, 2)]
-    # Keep features with non zero estimates only
-    features <- features[features != 0]
-    
-    # Fit the logistic regression model with selected features only
-    lambda.model <- InterUplift(data, treat, outcome, names(features), input = "best")
-    
-    
-    # Compute the qini coefficient and add to the list
-    if (validation == FALSE){
-      data$lambda.pred <- predict(lambda.model, data, treat)
-      lambda.perf <- PerformanceUplift(data, treat, outcome, "lambda.pred" , 
-                                       rank.precision = rank.precision, 
-                                       equal.intervals = equal.intervals, 
-                                       nb.group = nb.group)
-      lambda.qini[k] <- QiniArea(lambda.perf)
+    # All variables must be continuous. Before using this function, change 
+    # categorical variables to dummies.
+    inter.formula <- c()
+    for (k in seq(1:length(predictors))) {
+      inter.formula <- paste(inter.formula, paste(predictors[k], 
+                                                  treat, sep = ":"), sep = "+")
     }
-    if (validation == TRUE){
-      valid$lambda.pred <- predict(lambda.model, valid, treat)
-      lambda.perf <- PerformanceUplift(valid, treat, outcome, "lambda.pred" , rank.precision = rank.precision, 
-                                       equal.intervals = equal.intervals, 
-                                       nb.group = nb.group)
-      lambda.qini[k] <- QiniArea(lambda.perf)
+    formula <- as.formula(paste(paste(outcome, "~", treat, 
+                                      "+"), paste(predictors, collapse = "+"), 
+                                inter.formula))
+    
+    # Cross-validation
+    # Split data between train (data) and valid using SplitUplift() function
+    if (validation == TRUE) {
+      split <- SplitUplift(data, 1 - p, c(treat, outcome))
+      data <- split[[1]]
+      valid <- split[[2]]
     }
-  }
-  
-  best.model <- cbind(path[, c(1, 2)], lambda.qini)
-  # Take the model that maximizes the qini coefficient
-  max.qini <- which.max(best.model[, "lambda.qini"])[[1]]
-  best.model <- best.model[max.qini, ]
-  if (value == TRUE) {print(best.model)}
-  
-  # We also need to know which variables were selected
-  best.features <- path[path[, "lambda"]==best.model["lambda"], -c(1, 2, 3)]
-  best.features <- names(best.features[best.features != 0])
-  class(best.features) <- "BestFeatures"
-  
-  return(best.features)   
+    path <- LassoPath(data, formula)
+    # Keep paths of dimension > 0
+    path <- path[path[, "dimension"] > 0, ]
+    lambda.qini <- c()
+    for (k in 1:nrow(path)) {
+      features <- path[k, -c(1, 2)]
+      # Keep features with non zero estimates only
+      features <- features[features != 0]
+      
+      # Fit the logistic regression model with selected features only
+      lambda.model <- InterUplift(data, treat, outcome, names(features), 
+                                  input = "best")
+      
+      # Compute the qini coefficient and add to the list
+      if (validation == FALSE) {
+        data$lambda.pred <- predict(lambda.model, data, treat)
+        lambda.perf <- PerformanceUplift(data, treat, outcome, 
+                                         "lambda.pred", 
+                                         rank.precision = rank.precision, 
+                                         equal.intervals = equal.intervals, 
+                                         nb.group = nb.group)
+        if (length(lambda.perf[[1]]) == 1) { lambda.qini[k] <- 0}
+        else {lambda.qini[k] <- QiniArea(lambda.perf)}
+        
+      }
+      if (validation == TRUE) {
+        valid$lambda.pred <- predict(lambda.model, valid, treat)
+        lambda.perf <- PerformanceUplift(valid, treat, outcome, 
+                                         "lambda.pred", 
+                                         rank.precision = rank.precision, 
+                                         equal.intervals = equal.intervals, 
+                                         nb.group = nb.group)
+        if (length(lambda.perf[[1]]) == 1) { lambda.qini[k] <- 0}
+        else {lambda.qini[k] <- QiniArea(lambda.perf)}
+        
+      }
+    }
+    
+    best.model <- cbind(path[, c(1, 2)], lambda.qini)
+    
+    if (max(best.model[,3]) == 0) { 
+      warning("All models result in a Qini coefficient equal to 0. Please check LassoPath().")
+    }
+    # Take the model that maximizes the qini coefficient
+    max.qini <- which.max(best.model[,3])
+    best.model <- best.model[max.qini,]
+    if (value == TRUE) {
+      print(best.model)
+    }
+    
+    # We also need to know which variables were selected
+    best.features <- path[path[, "lambda"] == best.model["lambda"], -c(1, 2, 3)]
+    best.features <- names(best.features[best.features != 0])
+    class(best.features) <- "BestFeatures"
+    return(best.features)
 }
 
 # END FUN
--- pkg/R/LassoPath.R
+++ pkg/R/LassoPath.R
@@ -1,31 +1,37 @@
-LassoPath <- function(data, formula, nb.lambda=100){
-  
-  # LASSO path for penalized logistic regression.
-  #
-  # Args:
-  #   data: a data frame containing the treatment, the outcome and the predictors.
-  #   formula: an object of class "formula" (or one that can be coerced to that class): 
-  #            a symbolic description of the model to be fitted.
-  #   ... and default parameters.
-  #
-  # Returns:
-  #   A dataframe containing the coefficients values and the number of nonzeros coefficients.
-
-  X <- model.matrix(formula, data)
-  y <- model.frame(formula,data)[,1]
-  
-  glmnet.output <- glmnet(X, y, alpha=1, family="binomial", 
-                          nlambda = nb.lambda, 
-                          standardize = TRUE, intercept = FALSE)
-  
-  dimension <- glmnet.output$df
-  coeff <- t(as.matrix(glmnet.output$beta))
-  lambda <- glmnet.output$lambda
-  
-  path <- cbind(lambda, dimension, coeff)
-  class(path) <- "LassoPath"
-  
-  return(path)  
+LassoPath <- function (data, formula) {
+    
+    # LASSO path for penalized logistic regression.
+    #
+    # Args:
+    #   data: a data frame containing the treatment, the outcome and the predictors.
+    #   formula: an object of class "formula" (or one that can be coerced to that class): 
+    #            a symbolic description of the model to be fitted.
+    # Returns:
+    #   A dataframe containing the coefficients values and the number of nonzeros coefficients.
+    
+    X <- model.matrix(formula, data)
+    y <- model.frame(formula, data)[, 1]
+    
+    get_lambdas <- cv.glmnet(X, 
+                             y, 
+                             family="binomial",
+                             type.measure="auc",
+                             nfolds=3)
+    
+    glmnet.output <- glmnet(X, y, alpha = 1, family = "binomial", 
+                            lambda = get_lambdas$lambda, 
+                            standardize=TRUE, 
+                            intercept = FALSE,
+                            type.logistic = "modified.Newton")
+    
+    
+    dimension <- glmnet.output$df
+    coeff <- t(as.matrix(glmnet.output$beta))
+    lambda <- glmnet.output$lambda
+    path <- cbind(lambda, dimension, coeff)
+    
+    class(path) <- "LassoPath"
+    return(path)
 }
 
 # END FUN
